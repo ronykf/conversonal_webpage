3bf6861161112bea56c91c60dc293cd5c7c8370c
AlphredH <freddyhajj98@gmail.com>
2026-01-28 16:17:23 +0200

    Initial commit: Conversonal landing page with responsive design and mobile menu

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingPageController extends AbstractController
+{
+    #[Route('/', name: 'landing_page')]
+    public function index(): Response
+    {
+        return $this->render('landing_page/index.html.twig');
+    }
+}
